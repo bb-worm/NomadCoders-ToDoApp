022e898ec007a27f8c07370e02118ac4317195b6
현태룡 <xofyd99@naver.com>
2019-12-05 15:54:29 +0900

    view card UI2

--- App.js
+++ App.js
@@ -1,52 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView
+} from "react-native";
+import ToDo from "./ToDo";
 
 const { height, width } = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content"></StatusBar>
-      <Text style={styles.title}>To Do App</Text>
-      <View style={styles.card}>
-        <TextInput style={styles.input} placeholder={"New To Do"} />
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
+  render() {
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"></StatusBar>
+        <Text style={styles.title}>To Do App</Text>
+        <View style={styles.card}>
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._controlNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"} // 키패드에서 return 키를 '다음'으로 설정할지 '완료'로 설정할지 등
+            autoCorrect={false} // 자동완성 끄기
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
+    // 뒷배경
     flex: 1,
-    backgroundColor: '#f23657',
-    alignItems: 'center',
+    backgroundColor: "#f23657",
+    alignItems: "center"
   },
   title: {
-    color: 'white',
+    // title 박스
+    color: "white",
     fontSize: 30,
     marginTop: 40,
     marginBottom: 30,
-    fontWeight: '100',
+    fontWeight: "100"
   },
   card: {
-    backgroundColor:'white',
+    // card 박스
+    backgroundColor: "white",
     flex: 1,
-    width: width-25,
+    width: width - 25,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
     ...Platform.select({
+      // Platform에 따라 그림자 적용
       ios: {
         shadowColor: "rgb(50, 50, 50)",
         shadowOpacity: 0.5,
         shadowRadius: 5,
-        shadowOffset:{
+        shadowOffset: {
           height: -1,
           width: 0
         }
       },
       android: {
-        elevation: 5,
+        elevation: 5
       }
     })
+  },
+  input: {
+    // input 박스
+    padding: 20,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
   }
 });
