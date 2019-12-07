c57602dc113404e820183b35373f297d66173e9e
현태룡 <xofyd99@naver.com>
2019-12-07 17:45:00 +0900

    add event stoppropagation & change images

--- ToDo.js
+++ ToDo.js
@@ -91,7 +91,12 @@ export default class ToDo extends Component {
                 <Text style={styles.actionText}>✏️</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPressOut={() => deleteToDo(id)}>
+            <TouchableOpacity
+              onPressOut={event => {
+                event.stopPropagation();
+                deleteToDo(id);
+              }}
+            >
               <View style={styles.actionContainer}>
                 <Text style={styles.actionText}>❌</Text>
               </View>
@@ -102,7 +107,8 @@ export default class ToDo extends Component {
     );
   }
 
-  _toggleComplete = () => {
+  _toggleComplete = event => {
+    event.stopPropagation();
     const { id, isCompleted, completeToDo, uncompleteToDo } = this.props;
     if (isCompleted) {
       uncompleteToDo(id);
@@ -132,13 +138,15 @@ export default class ToDo extends Component {
     // });
   };
 
-  _startEditing = () => {
+  _startEditing = event => {
+    event.stopPropagation();
     this.setState({
       isEditing: true
     });
   };
 
-  _finishEditing = () => {
+  _finishEditing = event => {
+    event.stopPropagation();
     const { toDoValue } = this.state;
     const { id, updateToDo } = this.props;
     updateToDo(id, toDoValue);
